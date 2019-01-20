5da6dad4aeb1fd155770d052745f1f60e1c98073
Nishanth Raveendran <raveendn@mcmaster.ca>
2019-01-19 19:08:30 -0500

    play button fix

--- scripts.js
+++ scripts.js
@@ -1,30 +1,32 @@
 $('#playbutton').click(function() {
+  event.preventDefault();
   $('#welcome').slideUp(1600);
   $('#waiting').slideDown(1600);
 });
 
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+// function openForm() {
+//   document.getElementById("myForm").style.display = "block";
+// }
+//
+// function closeForm() {
+//   document.getElementById("myForm").style.display = "none";
+// }
 
 
 
 $(document).ready(function() {
 
 
-  $(function() {
-    var socket = io();
-    $('form').submit(function() {
-      socket.emit('chat message', $('#play').val());
-      $('#play').val('');
-      return false;
-    });
-    socket.on('chat message', function(msg) {
-      $('#playerlist').append($('<li>').text(msg));
-    });
-  });
+  // $(function() {
+  //   var socket = io();
+  //   $('form').submit(function() {
+  //     socket.emit('chat message', $('#play').val());
+  //     $('#play').val('');
+  //     return false;
+  //   });
+  //   socket.on('chat message', function(msg) {
+  //     $('#playerlist').append($('<li>').text(msg));
+  //   });
+  // });
+
 });
